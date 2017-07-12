57505d775e3e9d105193406517fa6a4469038f8f
Kel <codesmithk@gmail.com>
2017-07-11 21:25:29 -0400

    Version 1.0
    Updated organization name, removed copyright info from launch screen, changed input fields to clear when editing

--- PayCulator/ViewController.swift
+++ PayCulator/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  Payculator
 //
-// Created by K. Reid on 11/29/16.
-//  Copyright © 2016 K. Reid. All rights reserved.
+//  Created by K. Reid on 11/29/16.
+//  Copyright © 2017 KelCodesStuff All rights reserved.
 //
 
 import UIKit
