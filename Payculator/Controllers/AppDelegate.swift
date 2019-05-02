069720b07cb99cfa5e604105623b6e78c2682b45
Kelvin Reid <codesmithk@gmail.com>
2019-05-01 21:54:48 -0400

    Update AppDelegate.swift

--- Payculator/Controllers/AppDelegate.swift
+++ Payculator/Controllers/AppDelegate.swift
@@ -7,8 +7,6 @@
 //
 
 import UIKit
-import Fabric
-import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,10 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        Fabric.with([Crashlytics.self])
-        Fabric.sharedSDK().debug = true
-        
+                
         return true
     }
 
